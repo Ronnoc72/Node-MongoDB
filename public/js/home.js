4c3d951d53b6c999662ae5643acc72621f043130
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-02-04 14:14:18 -0700

    changes

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,7 @@ const wpm = document.getElementById('wpm');
 const missedLetter = document.getElementById('missed-letter');
 const mistakes = document.getElementById('mistakes');
 var index = 0;
+var startTime = 0;
 
 const words = document.getElementById('words');
 const wordList = [];
@@ -17,7 +18,7 @@ var started = false;
 
 window.addEventListener('keydown', (e) => {
     if (!started) {
-        var startTime = new Date().getTime();
+        startTime = new Date().getTime();
         started = true;
     }
     const key = e.key;
@@ -25,5 +26,9 @@ window.addEventListener('keydown', (e) => {
         index++;
     } else {
         console.log("mistake");
+    }
+    if (index + 1 === mainStr.length) {
+        var endTime = new Date().getTime()
+        console.log(endTime - startTime);
     }
 });
