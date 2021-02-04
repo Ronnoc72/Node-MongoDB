ffa6975bce5f05ae1a355b6031c5ad1f03ca3eb6
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-02-04 12:30:50 -0700

    main app

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000
-// importing the routing function from a different file.
-const router = require('./src/views/routes/routes.js')
 
 // how the debuging is happening.
 app.use(morgan('tiny'));
@@ -19,9 +17,8 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/', router);
 app.get('/', (req, res) => {
-    res.render('index', {list: ['a', 'b'], title: 'My site'});
+    res.render('index');
 });
 
 app.listen(3000, () => {
